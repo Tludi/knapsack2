9a8026a0b94500bc2d6d69f4469c78f698c6d88b
Tludi <tludi00@gmail.com>
2017-05-11 14:39:04 -0700

    Initial Commit

---
+++ knapsack2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  knapsack2
+//
+//  Created by manatee on 5/11/17.
+//  Copyright © 2017 diligentagility. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
